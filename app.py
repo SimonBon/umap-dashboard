c112865a94d674eb151594e9c3fc5a0b3732918b
SimonBonvino <simon.bonvino@gmail.com>
2023-11-16 10:26:56 +0100

    test

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.layout = html.Div([
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': [1, 2, 3, 5], 'y': [4, 1, 2, 6], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3, 4], 'y': [2, 4, 5, 7], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
